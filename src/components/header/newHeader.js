d9de692ec3e05921fe3a229ea50df76b1a7342b2
ziv-hagai <zivhag@gmail.com>
2022-11-08 11:33:59 +0200

    header

--- src/components/header/newHeader.js
+++ src/components/header/newHeader.js
@@ -4,35 +4,62 @@ import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import MicIcon from "@mui/icons-material/Mic";
 import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
 import Button from "@mui/material/Button";
-
+import logo from "../../assets/images/logo.jpg"
+import MenuIcon from "@mui/icons-material/Menu";
+import TollIcon from '@mui/icons-material/Toll';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 const NewHeader = () => {
-  return (
+    return (
         <div className="header">
-            <div className="balance">
-                <div className="balanceH">היתרה שלי</div>
-                <div className="balanceAmount">1200₪</div>
-            </div>
-            <form className="search-container active-search">
-                <div className="search-container__btn">
-                    <SearchOutlinedIcon />
+            <div className="top">
+                <div className='burger'>
+                    <MenuIcon
+                        fontSize="large"
+                        sx={{ color: 'var(--primary)' }}
+                    />
                 </div>
-                <input
-                    type="text"
-                    id="search-bar"
-                    className="search-container__input"
+
+                <img src={logo} />
+                <div className="balance">
+                    <div className="balanceH">היתרה שלי</div>
+                    <div className="balanceAmount">
+                        1200
+                        <TollIcon sx={{ color: 'var(--primary)' }}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="bottom">
+
+                <form className="search-container active-search">
+                    <div className="search-container__btn">
+                        <SearchOutlinedIcon />
+                    </div>
+                    <input
+                        type="text"
+                        id="search-bar"
+                        className="search-container__input"
                     // onChange={(e) => setSearchText(e.target.value)}
                     // onClick={openSearch}
                     // value={searchText}
-                />
-                <div className="mic-container__btn">
-                    <MicIcon />
+                    />
+                    <div className="mic-container__btn">
+                        <MicIcon />
+                    </div>
+                </form>
+                <div className="notificationBlock">
+                    <Button className="dropBtn">
+                        <NotificationsNoneIcon />
+                        <span className="subCount">3</span>
+                    </Button>
                 </div>
-            </form>
-            <div className="notificationBlock">
-                <Button className="dropBtn">
-                    <NotificationsNoneIcon />
-                    <span className="subCount">3</span>
-                </Button>
+                {/* <div className='coins_container'>
+                    <div className='coins'>
+                        <div className="">איך לצבור מטבעות?</div>
+                        <KeyboardArrowLeftIcon sx={{ color: 'var(--primary)' }}
+                        />
+                    </div>
+                </div> */}
             </div>
         </div>
     )
